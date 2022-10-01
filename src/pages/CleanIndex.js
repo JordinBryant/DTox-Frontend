22b61e0f762ea74cd1556342e746476707d71acc
Kelly Nelson <kelnel86@gmail.com>
2022-10-01 14:02:18 -0500

    created api call functions in main.js and creates loaded functions in food and clean index pages

--- src/pages/CleanIndex.js
+++ src/pages/CleanIndex.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const CleanIndex = () => {
-  return (
-    <div>CleanIndex</div>
-  )
+const CleanIndex = (props) => {
+  /////// LOADING FUNCTION TO DISPLAY JUST THE CHEMICAL NAMES ///////
+  const loaded = () => {
+    return props.clean.map((chem) => {
+        return <div key={chem._id} className="chemical">
+            <h4>{chem.chemical}</h4>
+        </div>
+    })
+}
+
+const loading = () => {
+    return <h1>Loading . . .</h1>
+}
+
+
+return (
+<div>
+    <h1>Chemicals Commonly Found in Cleaning Products</h1>
+    <h3>Select a chemical below to see a more!</h3>
+    {/* ///////// ternary statement to load the list ///////////*/}
+
+</div>
+)
 }
 
 export default CleanIndex;
--- src/pages/FoodIndex.js
+++ src/pages/FoodIndex.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
-const FoodIndex = () => {
+const FoodIndex = (props) => {
+    
+/////// LOADING FUNCTION TO DISPLAY JUST THE CHEMICAL NAMES ///////
+    const loaded = () => {
+        return props.food.map((chem) => {
+            return <div key={chem._id} className="chemical">
+                <h4>{chem.chemical}</h4>
+            </div>
+        })
+    }
+
+    const loading = () => {
+        return <h1>Loading . . .</h1>
+    }
+
+
   return (
-    <div>FoodIndex</div>
+    <div>
+        <h1>Chemicals Commonly Found in Foods</h1>
+        <h3>Select a chemical below to see a more!</h3>
+        {/* ///////// ternary statement to load the list ///////////*/}
+
+    </div>
   )
 }
 
