682c0c245e7b0eb43d54e4f771247897aa311e01
Jordin Bryant <jordintaylor095@gmail.com>
2022-10-04 17:04:04 -0400

    login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,48 @@
+import { login, logout } from '../services/firebase';
+import { Link } from 'react-router-dom'
 import React from 'react'
-import { Link } from 'react-router-dom';
-
-
 const Header = () => {
-  return (
-    <nav classname="nav">
-        <Link to="/">
-            <div>HOME</div>
-        </Link>
-        <Link to="/food">
-            <div>FOOD</div>
-        </Link>
-        <Link to="/clean">
-            <div>CLEANING PRODUCTS</div>
-        </Link>
-        <Link to="/other">
-            <div>OTHER PRODUCTS</div>
-        </Link>
-    </nav>
-  )
+    return (
+        <nav classname="nav"
+            style={{
+                display:'flex',
+                alignItems:'center',
+                justifyContent: 'space-between'
+            }}
+            >
+            <Link to="/">
+                <div>HOME</div>
+            </Link>
+            <Link to="/food">
+                <div>FOOD</div>
+            </Link>
+            <Link to="/clean">
+                <div>CLEANING PRODUCTS</div>
+            </Link>
+            <Link to="/other">
+                <div>OTHER PRODUCTS</div>
+            </Link>
+           
+            <div style={{ display:'flex'}}>
+                <div
+                    onClick={login}
+                    style={{
+                        cursor: 'pointer',
+                        marginRight: 10
+                    }}>
+                    Login
+                </div>
+                <div
+                    onClick={logout}
+                    style={{
+                        cursor: 'pointer',
+                        marginRight: 10
+                    }}>
+                    logout
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 
